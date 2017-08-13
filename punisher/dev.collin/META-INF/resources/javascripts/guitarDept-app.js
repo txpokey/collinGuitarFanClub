7420467c49acf2486c65045c107347b2afca72cd
Bob Makowski <tritonpi-git0@yahoo.com>
2017-08-13 13:28:46 -0500

    broken carousel: work around with ng-repeat looking for a {{slide}}.jpg. also centralized the list of slides unto a variable set in the header controller

--- punisher/dev.collin/META-INF/resources/javascripts/guitarDept-app.js
+++ punisher/dev.collin/META-INF/resources/javascripts/guitarDept-app.js
@@ -3,6 +3,8 @@
 
     app.controller('HeaderController', [ '$http' , function($http){
         var header = this;
+        header.carouselSlideDeck = [0,1,2];
+        console.log("controller loaded:> " + header.carouselSlideDeck);
         header.targets = [];
         $http.get('../json/header-controller.json').then(function(response){
             header.targets = response.data;
@@ -10,6 +12,7 @@
             console.log(badResult)
         }) ;
     }]);
+    console.log("where am i?");
 
     // app.controller('ReviewController', function() {
     //     this.review = {};
