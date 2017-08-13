ca77acad4df0d313578a4a400c1678b6127c71c5
Bob Makowski <tritonpi-git0@yahoo.com>
2017-08-13 14:24:25 -0500

    T: fixed carousel: clean ups from work to fix carousel

--- punisher/dev.collin/META-INF/resources/javascripts/guitarDept-app.js
+++ punisher/dev.collin/META-INF/resources/javascripts/guitarDept-app.js
@@ -4,19 +4,12 @@
     app.controller('HeaderController', [ '$http' , function($http){
         var header = this;
         header.carouselSlideDeck = [0,1,2];
-        console.log("controller loaded:> " + header.carouselSlideDeck);
         header.targets = [];
         $http.get('../json/header-controller.json').then(function(response){
             header.targets = response.data;
         },function (badResult){
             console.log(badResult)
         }) ;
-        // console.log("running inside of controller");
-        // $('.carousel').carousel({
-        //     interval: 300
-        // });
-        // $('.carousel').carousel('cycle');
-        // console.log("controller completing");
     }]);
     console.log("running outside of controller");
 
