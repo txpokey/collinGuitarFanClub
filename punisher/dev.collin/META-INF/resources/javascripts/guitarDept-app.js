59fdc7655eff29456797fd4063d85d1a0525a834
Bob Makowski <tritonpi-git0@yahoo.com>
2017-08-14 22:56:58 -0500

    T: sci with ng-include: cleanup: moved biography code into bioController

--- punisher/dev.collin/META-INF/resources/javascripts/guitarDept-app.js
+++ punisher/dev.collin/META-INF/resources/javascripts/guitarDept-app.js
@@ -1,6 +1,6 @@
 (function() {
     var app = angular.module('guitarDept', ['guitarDept-directives', 'ngAnimate', 'ngSanitize']);
-    app.config(function($sceDelegateProvider) {
+    app.config(function($sceDelegateProvider) { // needs 'ngSanitize' : but does not work because of CORS rules anyway
         $sceDelegateProvider.resourceUrlWhitelist([
             // Allow same origin resource loads.
             'self',
@@ -13,20 +13,6 @@
         app.controller('HeaderController', [ '$http' , function($http){
         var header = this;
         header.carouselSlideDeck = [0,1,2];
-//
-        header.musicDepartmentURL = new Object();
-        header.musicDepartmentURL.fernand = "/html/bio/fernand/FVera_Bio.html";
-        header.musicDepartmentURL.olga    = "/html/bio/olga/index.html";
-        var fv = {
-           name : 'fernand' , url : header.musicDepartmentURL.fernand
-        } ;
-        var ov = {
-           name : 'olga'   , url : header.musicDepartmentURL.olga
-        } ;
-        header.musicDepartmentURL.ALL = [ fv , ov ] ;
-        header.musicDepartmentURL.fv = fv ;
-        header.musicDepartmentURL.ov = ov ;
-//
         header.targets = [];
         $http.get('../json/header-controller.json').then(function(response){
             header.targets = response.data;
@@ -34,15 +20,16 @@
             console.log(badResult)
         }) ;
     }]);
-    console.log("running outside of controller");
-
-    // app.controller('ReviewController', function() {
-    //     this.review = {};
-    //
-    //     this.addReview = function(product) {
-    //         product.reviews.push(this.review);
-    //
-    //         this.review = {};
-    //     };
-    // });
+    // console.log("running outside of controller");
+    app.controller('BioController', [ '$http' , function($http){
+        var bioController = this;
+        bioController.guitarDepartmentFaculty = new Object();
+        var fv = {
+            name : 'fernand' , url : bioController.guitarDepartmentFaculty.fernand = "/html/bio/fernand/FVera_Bio.html"
+        } ;
+        var ov = {
+            name : 'olga'    , url : bioController.guitarDepartmentFaculty.olga    = "/html/bio/olga/index.html"
+    } ;
+        bioController.guitarDepartmentFaculty.ALL = [ fv , ov ] ;
+    }]);
 })();
