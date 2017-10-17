2cc6ac16531cd0fe7a5ea6837819c64ab34008ac
Bob Makowski <tritonpi-git0@yahoo.com>
2017-10-16 20:26:18 -0500

    BRANCH == dev.collin/checkpoint/baseline-8/demo-2-0/170904-0
    accomplishments:
    
    T: renamed .json to .html so collin firewall would pass the files
    
    new feature : meta data :
    F: data : added a couple open graph meta tags in effort for facebook thumbnailing. not working yet
    
    .................................
    .................................

--- punisher/dev.collin/META-INF/resources/assets/javascripts/guitarDept-app.js
+++ punisher/dev.collin/META-INF/resources/assets/javascripts/guitarDept-app.js
@@ -14,7 +14,7 @@
         var header = this;
         header.carouselSlideDeck = [0,1,2];
         header.targets = [];
-        $http.get('../assets/json/header-controller.json').then(function(response){
+        $http.get('../assets/json/header-controller.html').then(function(response){
             header.targets = response.data.targets;
             header.title = response.data.title;
         },function (badResult){
@@ -24,7 +24,7 @@
     app.controller('TeacherBiographyController', [ '$http' , function($http){
         var bioController = this;
         bioController.guitarDepartmentFaculty = [];
-        $http.get('../assets/json/teacherBiography-controller.json').then(function (response) {
+        $http.get('../assets/json/teacherBiography-controller.html').then(function (response) {
             bioController.guitarDepartmentFaculty = response.data;
         }, function (badResult) {
             console.log(badResult)
@@ -33,7 +33,7 @@
     app.controller('GuitarEventsController', [ '$http' , function($http){
         var eventsController = this;
         eventsController.guitarProgramEvents = [];
-        $http.get('../assets/json/guitarEvents-controller.json').then(function (response) {
+        $http.get('../assets/json/guitarEvents-controller.html').then(function (response) {
             eventsController.guitarProgramEvents = response.data;
         }, function (badResult) {
             console.log(badResult)
@@ -42,13 +42,13 @@
     app.controller('GuitarCoursesBySchoolTermController', [ '$http' , function($http){
         var catalogController = this;
         catalogController.guitarProgramCatalog = [];
-        $http.get('../assets/json/courseWare/2017/fall/current/musicCourses.json').then(function (response) {
+        $http.get('../assets/json/courseWare/2017/fall/current/musicCourses.html').then(function (response) {
             catalogController.guitarProgramCatalog = response.data;
         }, function (badResult) {
             console.log(badResult)
         });
         catalogController.guitarProgramCatalogBySection = [];
-        $http.get('../assets/json/courseWare/2017/fall/current/musicCoursesBySection.json').then(function (response) {
+        $http.get('../assets/json/courseWare/2017/fall/current/musicCoursesBySection.html').then(function (response) {
             catalogController.guitarProgramCatalogBySection = response.data;
         }, function (badResult) {
             console.log(badResult)
@@ -58,7 +58,7 @@
     app.controller('GuitarLinksController', [ '$http' , function($http){
         var linksController = this;
         linksController.guitarProgramLinks = [];
-        $http.get('../assets/json/links-controller.json').then(function (response) {
+        $http.get('../assets/json/links-controller.html').then(function (response) {
             linksController.guitarProgramLinks = response.data;
         }, function (badResult) {
             console.log(badResult)
@@ -67,7 +67,7 @@
     app.controller('GuitarNewsController', [ '$http' , function($http){
         var newsController = this;
         newsController.guitarProgramNews = [];
-        $http.get('../assets/json/news-controller.json').then(function (response) {
+        $http.get('../assets/json/news-controller.html').then(function (response) {
             newsController.guitarProgramNews = response.data;
         }, function (badResult) {
             console.log(badResult)
@@ -76,7 +76,7 @@
     app.controller('GuitarTextbooksController', [ '$http' , function($http){
         var textbooksController = this;
         textbooksController.textbooks = [];
-        $http.get('../assets/json/textbooks-controller.json').then(function (response) {
+        $http.get('../assets/json/textbooks-controller.html').then(function (response) {
             textbooksController.textbooks = response.data;
         }, function (badResult) {
             console.log(badResult)
@@ -85,12 +85,12 @@
     app.controller('GuitarPerformanceController', [ '$http' , function($http){
         var performanceController = this;
         performanceController.channelQuery = [];
-        $http.get('../assets/json/you-tube.playlistsByChannel.query.json').then(function (response) {
+        $http.get('../assets/json/you-tube.playlistsByChannel.query.html').then(function (response) {
             performanceController.channelQuery = response.data;
         }, function (badResult) {
             console.log(badResult)
         });
-        $http.get('../assets/json/you-tube.playlist.query.json').then(function (response) {
+        $http.get('../assets/json/you-tube.playlist.query.html').then(function (response) {
             performanceController.playlistQuery = response.data;
             for (i =0 , q = performanceController.playlistQuery; i < q.length; i++) {
                 q[i].playlistId = q[i].items[0].snippet.playlistId;
@@ -102,7 +102,7 @@
     app.controller('GuitarFooterController', [ '$http' , function($http){
         var footerController = this;
         footerController.guitarProgramFooter = [];
-        $http.get('../assets/json/footer-controller.json').then(function (response) {
+        $http.get('../assets/json/footer-controller.html').then(function (response) {
             footerController.guitarProgramFooter = response.data;
         }, function (badResult) {
             console.log(badResult)
