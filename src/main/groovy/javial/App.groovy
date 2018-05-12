a572a71a49c014e88313494b75400c4360a76eb5
Bob Makowski <tritonpi-git0@yahoo.com>
2018-05-12 17:36:47 -0500

    F:F: added Application.groovy. I can build this via spring jar (without Application.groovy), but gradle is not putting my resources into META-INF and so springboot is NOT picking up and setting the index.html as the welcome page

--- src/main/groovy/javial/App.groovy
+++ src/main/groovy/javial/App.groovy
@@ -1,12 +1,18 @@
+package javial
 /**
  * Created by mak on 7/10/17. updated 5/11/18
  */
-@Controller class App { 
+import org.springframework.stereotype.Controller
+
+@Controller class App {
 		// gradle init artifact
     String getGreeting() {
         return 'Hello world.'
     }
-		// gradle init artifact
+    /*
+     * NOT needed or used when executable jar built via:
+     *     spring jar <jarName> META-INF src/main/groovy
+     */
     static void main(String[] args) {
         println new App().greeting
     }
---
+++ src/main/groovy/javial/Application.groovy
@@ -0,0 +1,30 @@
+package javial
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.builder.SpringApplicationBuilder
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer
+
+//import org.springframework.boot.SpringApplication
+//import org.springframework.boot.autoconfigure.SpringBootApplication
+//
+//@SpringBootApplication
+//public class Application{
+//
+//    public static void main(String[] args) {
+//        SpringApplication.run(Application.class, args);
+//    }
+//}
+
+@SpringBootApplication
+public class Application extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(Application.class)
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args)
+    }
+}
