a45d726f3a5ddd1ab2c3cb996bf2fe2198f765a1
Bob Makowski <tritonpi-git0@yahoo.com>
2018-05-12 20:52:06 -0500

    T:T: gradle is now putting my resources into META-INF and so springboot picks the index.html as the welcome page. angular application runs fine

--- src/main/groovy/javial/Application.groovy
+++ src/main/groovy/javial/Application.groovy
@@ -5,17 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.builder.SpringApplicationBuilder
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer
 
-//import org.springframework.boot.SpringApplication
-//import org.springframework.boot.autoconfigure.SpringBootApplication
-//
-//@SpringBootApplication
-//public class Application{
-//
-//    public static void main(String[] args) {
-//        SpringApplication.run(Application.class, args);
-//    }
-//}
-
 @SpringBootApplication
 public class Application extends SpringBootServletInitializer {
 
